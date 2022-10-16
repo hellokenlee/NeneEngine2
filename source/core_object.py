3e8a9e1e8052c4e08ba3a9312784728af3023bf4
KenLee <ken4000kl@gmail.com>
2022-10-16 18:57:11 +0800

    Add core_render, core_object module; Enhance NBT;

---
+++ source/core_object.py
@@ -0,0 +1,8 @@
+# -*- coding=utf-8 -*-
+"""
+    Nene Module File
+"""
+
+DEPENDENCY = []
+
+EXTERNAL_LIB = []
--- toolchain/common/tool_base.py
+++ toolchain/common/tool_base.py
@@ -12,11 +12,16 @@ class ToolBase(object):
 	NAME = "Unknown"
 	SOURCE = "source"
 	RELATIVE_TO_ENGINE_ROOT = "../.."
+	RELATIVE_TO_TEMPLATE_FILE = "nene.py.template"
 
 	def __init__(self):
 		super(ToolBase, self).__init__()
 		self.engine_root = os.path.abspath(os.path.join(os.path.dirname(__file__), self.RELATIVE_TO_ENGINE_ROOT))
 		self.source_root = os.path.join(self.engine_root, self.SOURCE)
+		template_path = os.path.join(os.path.dirname(__file__), self.RELATIVE_TO_TEMPLATE_FILE)
+		self.template_content = ""
+		with open(template_path, "r") as fp:
+			self.template_content = fp.readlines()
 		pass
 
 	def run(self, args: list[str]):
@@ -32,24 +37,23 @@ class ToolBase(object):
 	def get_proj_path(self, proj):
 		return os.path.join(self.source_root, proj)
 
-	def dependency(self, proj: str) -> list[str]:
+	def get_or_create_nene_module(self, proj: str):
 		nene_file = os.path.join(self.source_root, "%s.py" % proj)
-		if os.path.exists(nene_file):
-			nene = SourceFileLoader(proj, nene_file).load_module()
-			if hasattr(nene, "DEPENDENCY"):
-				return nene.DEPENDENCY
-		else:
-			# print("    [Warning] Failed to find nene proj file: %s.py" % nene_file)
-			pass
+		if not os.path.exists(nene_file):
+			with open(nene_file, "w") as fp:
+				fp.writelines(self.template_content)
+				pass
+		nene = SourceFileLoader(proj, nene_file).load_module()
+		return nene
+
+	def dependency(self, proj: str) -> list[str]:
+		nene = self.get_or_create_nene_module(proj)
+		if hasattr(nene, "DEPENDENCY"):
+			return nene.DEPENDENCY
 		return []
 
 	def external_lib(self, proj: str) -> list[str]:
-		nene_file = os.path.join(self.source_root, "%s.py" % proj)
-		if os.path.exists(nene_file):
-			nene = SourceFileLoader(proj, nene_file).load_module()
-			if hasattr(nene, "EXTERNAL_LIB"):
-				return nene.EXTERNAL_LIB
-		else:
-			# print("    [Warning] Failed to find nene proj file: %s.py" % nene_file)
-			pass
+		nene = self.get_or_create_nene_module(proj)
+		if hasattr(nene, "EXTERNAL_LIB"):
+			return nene.EXTERNAL_LIB
 		return []
