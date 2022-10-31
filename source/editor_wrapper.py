f828232b76fe3f7a3d7bcf70576d07db99ee2646
KenLee <ken4000kl@gmail.com>
2022-10-31 18:13:32 +0800

    Add editor wrapper;

--- source/editor_wrapper.py
+++ source/editor_wrapper.py
@@ -3,5 +3,6 @@
 DEPENDENCY = ["core", "engine"]
 
 EXTERNAL = {
-	"python3.10": ["python3.lib"]
+	"python3.10": ["python3.lib"],
+	"qt5.15.2": ["Qt5Core.lib", "Qt5Gui.lib", "Qt5Widgets.lib"]
 }
--- source/editor_wrapper/editor_start.cpp
+++ source/editor_wrapper/editor_start.cpp
@@ -2,12 +2,15 @@
 
 #include "editor_start.h"
 #include "core/core.h"
+#include "QtWidgets/QApplication"
 
 DECLARE_LOG_CATEGORY(editor)
 
 void editor_start::init()
 {
     LOG(editor, info, TEXT("Hello Nene Engine!"));
+    QCoreApplication* app = QApplication::instance();
+    LOG(editor, info, TEXT("QApp: %x"), app);
 }
 
 void editor_start::finalize()
