0dcb02b13576e09433160a7a2d6e3f31658f7407
KenLee <ken4000kl@gmail.com>
2025-09-24 10:46:49 +0800

    Add DirectXShaderCompiler v1.8.2505;

---
+++ extern/dxc/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding=utf-8 -*-
+# __author__ = "KenLee"
+# __email__ = "hellokenlee@163.com"
+
+from script.builder.common.external_library import ExternalLibrary
+
+
+class Dxc(ExternalLibrary):
+	pass
