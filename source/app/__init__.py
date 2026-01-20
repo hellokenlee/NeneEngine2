2d8cee2a02a9393e478ff1d2012945683472d418
KenLee <ken4000kl@gmail.com>
2026-01-20 20:46:16 +0800

    MOD: Add SDL3;

--- source/app/__init__.py
+++ source/app/__init__.py
@@ -6,6 +6,8 @@ from source import *
 from source.core import Core
 from source.core_object import CoreObject
 from source.engine import Engine
+from extern.sdl3 import Sdl3
+from extern.sdl3_image import Sdl3Image
 
 
 class App(NeneModule):
@@ -15,5 +17,8 @@ class App(NeneModule):
 		self.build_target = BuildTarget.EXE
 		self.module_dependencies.extend(
 			[Core, CoreObject, Engine]
+		)
+		self.external_dependencies.extend(
+			[Sdl3, Sdl3Image]
 		)
 		pass
