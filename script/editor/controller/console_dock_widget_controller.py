550ec02e0041b1a330ddc237eaabf66b6dc2a77b
KenLee <ken4000kl@gmail.com>
2026-03-27 12:17:05 +0800

    [Editor] Add python exec;

--- script/editor/controller/console_dock_widget_controller.py
+++ script/editor/controller/console_dock_widget_controller.py
@@ -5,8 +5,10 @@
 import logging
 import html
 import re
+import traceback
 
-from PySide6.QtWidgets import QTextBrowser
+from PySide6.QtCore import QObject, QEvent, Qt
+from PySide6.QtWidgets import QTextBrowser, QLineEdit
 
 from script.editor.common.log import global_logger, global_log_formatter
 from script.editor.controller.dock_widget_controller import DockWidgetController
@@ -108,14 +110,104 @@ class PythonLogHandler(logging.Handler):
 		pass
 
 
+class _CommandLineHistoryFilter(QObject):
+	def __init__(self, controller, parent=None):
+		super().__init__(parent)
+		self._controller = controller
+
+	def eventFilter(self, watched, event):
+		if event.type() == QEvent.Type.KeyPress:
+			if event.key() == Qt.Key.Key_Up:
+				self._controller._restore_prev_command()
+				return True
+			if event.key() == Qt.Key.Key_Down:
+				self._controller._restore_next_command()
+				return True
+		return False
+
+
 class ConsoleDockWidgetController(DockWidgetController):
 
 	UI_FILE = "console_dock_widget.ui"
 
 	def __init__(self):
 		super(ConsoleDockWidgetController, self).__init__()
+		self.command_line_edit = self.ui.findChild(QLineEdit, "commandLineEdit")
 		self.log_text_browser = self.ui.findChild(QTextBrowser, "logTextBrowser")
+		self._python_globals = {"__builtins__": __builtins__}
+		self._python_locals = {"controller": self}
+		self._command_history = []
+		self._history_cursor = 0
+		self._history_draft = ""
+		self._command_history_filter = None
 		#
 		self.engine_log_handler = EngineLogHandler(self.log_text_browser)
 		self.python_log_handler = PythonLogHandler(self.log_text_browser)
+		if self.command_line_edit:
+			self._command_history_filter = _CommandLineHistoryFilter(self, self.command_line_edit)
+			self.command_line_edit.installEventFilter(self._command_history_filter)
+			self.command_line_edit.returnPressed.connect(self._on_command_line_return_pressed)
+			self.command_line_edit.textEdited.connect(self._on_command_text_edited)
+		pass
+
+	def _on_command_text_edited(self, _):
+		self._history_cursor = len(self._command_history)
+		if self.command_line_edit is not None:
+			self._history_draft = self.command_line_edit.text()
+		pass
+
+	def _restore_prev_command(self):
+		if self.command_line_edit is None or not self._command_history:
+			return
+		if self._history_cursor == len(self._command_history):
+			self._history_draft = self.command_line_edit.text()
+		if self._history_cursor > 0:
+			self._history_cursor -= 1
+		command = self._command_history[self._history_cursor]
+		self.command_line_edit.setText(command)
+		self.command_line_edit.setCursorPosition(len(command))
+		pass
+
+	def _restore_next_command(self):
+		if self.command_line_edit is None or not self._command_history:
+			return
+		last_index = len(self._command_history) - 1
+		if self._history_cursor < last_index:
+			self._history_cursor += 1
+			command = self._command_history[self._history_cursor]
+		else:
+			self._history_cursor = len(self._command_history)
+			command = self._history_draft
+		self.command_line_edit.setText(command)
+		self.command_line_edit.setCursorPosition(len(command))
+		pass
+
+	def _on_command_line_return_pressed(self):
+		if self.command_line_edit is None or self.log_text_browser is None:
+			return
+		command = self.command_line_edit.text().strip()
+		if not command:
+			return
+
+		self._command_history.append(command)
+		self._history_cursor = len(self._command_history)
+		self._history_draft = ""
+		self.command_line_edit.clear()
+		_append_colored_log(self.log_text_browser, ">>> %s" % command, logging.DEBUG)
+
+		try:
+			try:
+				code = compile(command, "<console>", "eval")
+				result = eval(code, self._python_globals, self._python_locals)
+				if result is not None:
+					_append_colored_log(self.log_text_browser, repr(result), logging.INFO)
+			except SyntaxError:
+				try:
+					code = compile(command, "<console>", "exec")
+					exec(code, self._python_globals, self._python_locals)
+				except SyntaxError as syntax_error:
+					raise syntax_error from None
+		except Exception:
+			for line in traceback.format_exc().rstrip().splitlines():
+				_append_colored_log(self.log_text_browser, line, logging.ERROR)
 		pass
