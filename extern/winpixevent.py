0633b3ebc12140e2cb7ee84a36de6575ada13cd0
KenLee <ken4000kl@gmail.com>
2026-06-11 14:06:53 +0800

    Remove invalid;

--- extern/winpixevent.py
+++
@@ -1,8 +0,0 @@
-# -*- coding=utf-8 -*-
-# __author__ = "KenLee"
-# __email__ = "hellokenlee@163.com"
-
-from script.builder.common.vcpkg_package import VcpkgPackage
-
-class WinPixEvent(VcpkgPackage):
-	pass
--- source/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding=utf-8 -*-
-# __author__ = "KenLee"
-# __email__ = "hellokenlee@163.com"
-
-from script.builder.common.nene_project import *
-
-
-class NeneEngine2(NeneProject):
-	pass
